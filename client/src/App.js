bf79f52b91dee363050fe14957eed168a05674ed
Daniel <danisan9490@gmail.com>
2020-10-04 11:38:32 +0100

    #3 Add Weather Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Form from './components/Form';
+import Clima from './components/Clima';
 
 function App() {
 
@@ -10,19 +11,21 @@ function App() {
     pais: ''
   });
   const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
 
   const { ciudad, pais } = busqueda;
 
   useEffect(() => {
     const consultarAPI = async () => {
       if (consultar) {
-        const appId = '';
+        const appId = '3474cbad59a643bcfa72ef477262b036';
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
-
-        console.log(resultado);
+        console.log(resultado)
+        guardarResultado(resultado);
+        guardarConsultar(false);
       }
     }
     consultarAPI();
@@ -44,8 +47,10 @@ function App() {
               />
             </div>
             <div className="col m6 s12">
-              2
-                    </div>
+              <Clima
+                resultado={resultado}
+              />
+            </div>
           </div>
         </div>
       </div>
