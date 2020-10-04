8b79d8a8637a49fcf224ed88977e854a7601cb03
Daniel <danisan9490@gmail.com>
2020-10-04 12:22:39 +0100

    Updates

---
+++ precache-manifest.c04e08bc84698862117aed9d08b8bb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f448e9e3f3aacd38cd4c507689f3fc9",
+    "url": "/city-weather/index.html"
+  },
+  {
+    "revision": "4053497d5fe64c501c54",
+    "url": "/city-weather/static/css/main.43a6801c.chunk.css"
+  },
+  {
+    "revision": "f91d7071d767795cb3e4",
+    "url": "/city-weather/static/js/2.26b5fa54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/city-weather/static/js/2.26b5fa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4053497d5fe64c501c54",
+    "url": "/city-weather/static/js/main.2cbd6c4c.chunk.js"
+  },
+  {
+    "revision": "687635a87858e5c55ade",
+    "url": "/city-weather/static/js/runtime-main.b6382b92.js"
+  }
+]);
